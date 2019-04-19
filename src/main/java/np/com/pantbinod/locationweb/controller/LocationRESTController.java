eedcd10bf2ae8b2e3f2539b2dce77c7394003a1b
Binod Pant <bpant@deerhold.org>
2019-04-19 14:01:54 +0545

    rest crud complted

---
+++ src/main/java/np/com/pantbinod/locationweb/controller/LocationRESTController.java
@@ -0,0 +1,40 @@
+package np.com.pantbinod.locationweb.controller;
+
+
+import np.com.pantbinod.locationweb.modal.Location;
+import np.com.pantbinod.locationweb.resp.LocationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/locations")
+public class LocationRESTController {
+
+    @Autowired
+    private LocationRepository locationRepository;
+
+
+    @GetMapping
+    public List<Location> getLocations(){
+        return locationRepository.findAll();
+    }
+
+    @PostMapping
+    public Location saveLocation(@RequestBody Location location){
+        return locationRepository.save(location);
+    }
+
+    @PutMapping
+    public Location updateLocation(@RequestBody Location location){
+        return locationRepository.save(location);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteLocation(@PathVariable("id") int id){
+        Location location = new Location();
+        location.setId(id);
+        locationRepository.delete(location);
+    }
+}
